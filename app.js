b274406dc108c7de15b1ac5c8d8e44d21351a51c
CaueRalhoNunes <76968286+CaueRalhoNunes@users.noreply.github.com>
2022-08-18 10:20:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ const port = process.env.PORT;
 
 app.listen(port, (req, res) => {
     console.log(`Server is Running on http://localhost:${port}`);
-    console.log('hello world')
+    console.log('hello world...')
 });
